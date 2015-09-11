dabf66d50d36add383fe30eafb81bca8c5db9183
Jeff Schwartz <jefftschwartz@gmail.com>
2015-09-11 08:48:13 -0400

    Viewport now cannot scroll when colorbox is shown.

--- js/blogpage.js
+++ js/blogpage.js
@@ -7,7 +7,24 @@
             description = $this.find(".panel-title").text(),
             img = $this.find(".panel-body img");
         $(img).on("click", function(){
-            $.colorbox({ width: "100%", height: "100%", href: $(img).attr("src"), title: description});
+            $.colorbox({
+                width: "100%",
+                height: "100%",
+                href: $(img).attr("src"),
+                title: description,
+                onComplete: function(){
+                    //prevent scrolling
+                    $("html, body").css({
+                        "overflow": "hidden"
+                    });
+                },
+                onClosed: function(){
+                    // restore scrolling
+                    $("html, body").css({
+                        "overflow": ""
+                    });
+                }
+            });
         });
     });
 }());
